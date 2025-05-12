753517378e88b8f0d75631607179bcd6891939b3
elagil <elagil@takanome.de>
2025-05-12 20:52:09 +0200

    ci: simplify build

--- .github/ci/build.sh
+++ .github/ci/build.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-for dir in firmware/loeti;
-do
-    pushd $dir
-    cargo fmt
-    cargo clippy -- -D warnings
-    cargo build --release
-    popd
-done
-
-for dir in firmware/loeti
-do
-    pushd $dir
-    cargo clippy -- -D warnings
-    popd
-done
+pushd firmware/loeti
+cargo fmt
+cargo clippy -- -D warnings
+cargo build --release
+popd
